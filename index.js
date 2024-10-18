8205ffead66767880345e607e8c4fad096b984ad
wholespace214 <wholespace.214@gmail.com>
2024-10-18 20:22:04 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const Telegram = require("node-telegram-bot-api");
+const { ethers } = require("ethers");
+
+require("dotenv").config();
+
+const { Account } = require("./lib/account");
+
+const main = () => {
+  const telegramBot = new Telegram(process.env.TELEGRAM_BOT, { polling: true });
+  let accounts = [];
+
+  //   telegramBot.on("message", (msg) => {
+  //     console.log(msg.text);
+  //   });
+
+  telegramBot.onText(/\/(\w+)(?:\s(\w+))?/, async (msg, match) => {
+    const userId = msg.from.id;
+    const command = match[1];
+
+    if (command === "add") {
+      const account = new Account(match[2]);
+      accounts.push(account);
+    } else if (command === "balance") {
+      const account = new Account(match[2]);
+      const balance = await account.getBalance();
+      telegramBot.sendMessage(msg.chat.id, balance);
+    } else if (command === "list") {
+      let text = "";
+      for (let account of accounts) {
+        text += `Address: ${account.address}` + "\n";
+        text += `Balance: ${account.balance}` + "\n";
+        text += "\n";
+      }
+      telegramBot.sendMessage(msg.chat.id, text, {
+        parse_mode: "Markdown",
+      });
+    }
+  });
+
+  setInterval(async () => {
+    for (let account of accounts) {
+      const newBalance = await account.getBalance();
+      if (Number(newBalance) > account.balance) {
+        account.setBalance(newBalance);
+        telegramBot.sendMessage(process.env.CHAT_ID, newBalance);
+      }
+    }
+  }, 1000);
+};
+
+main();
