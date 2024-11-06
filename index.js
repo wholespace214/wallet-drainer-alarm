b2ed11c17378721efdd89bdfdebc2126ea9a7623
wholespace214 <wholespace.214@gmail.com>
2024-11-06 14:27:19 +0900

    update the bot

--- index.js
+++ index.js
@@ -4,26 +4,33 @@ const { ethers } = require("ethers");
 require("dotenv").config();
 
 const { Account } = require("./lib/account");
+const { loadAddressList, saveAddressList } = require("./lib/file");
 
-const main = () => {
+const main = async () => {
   const telegramBot = new Telegram(process.env.TELEGRAM_BOT, { polling: true });
   let accounts = [];
 
-  //   telegramBot.on("message", (msg) => {
-  //     console.log(msg.text);
-  //   });
+  const list = await loadAddressList();
+
+  for (let item of list) {
+    const account = new Account(item);
+    accounts.push(account);
+  }
 
   telegramBot.onText(/\/(\w+)(?:\s(\w+))?/, async (msg, match) => {
     const userId = msg.from.id;
     const command = match[1];
 
     if (command === "add" && match[2]) {
+      list.push(match[2]);
       const account = new Account(match[2]);
       accounts.push(account);
+      saveAddressList(list);
     } else if (command === "balance" && match[2]) {
       if (ethers.utils.isAddress(match[2])) {
         const account = new Account(match[2]);
         const balance = await account.getBalance();
+        account.setBalance(balance);
         telegramBot.sendMessage(msg.chat.id, balance);
       } else {
         telegramBot.sendMessage(msg.chat.id, "Please input correct Address");
@@ -44,9 +51,14 @@ const main = () => {
   setInterval(async () => {
     for (let account of accounts) {
       const newBalance = await account.getBalance();
-      if (Number(newBalance) > account.balance) {
+      if (Number(newBalance) !== Number(account.balance)) {
         account.setBalance(newBalance);
-        telegramBot.sendMessage(process.env.CHAT_ID, newBalance);
+        let text = "";
+        text += `Address: ${account.address}` + "\n";
+        text += `Balance: ${newBalance}` + "\n";
+        telegramBot.sendMessage(process.env.CHAT_ID, text, {
+          parse_mode: "Markdown",
+        });
       }
     }
   }, 1000);
